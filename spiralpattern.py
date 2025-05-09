e78ab52b4bc48717299102ea342642dc1ae3913d
nupurkarim88 <nupurkarim88@gmail.com>
2025-05-09 22:22:51 +0600

    spiralpattern

---
+++ spiralpattern.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+board = turtle.Turtle()
+
+#Square
+board.forward(100) 
+board.right(90)
+board.forward(100)
+board.left(90)
+board.forward(100) 
+board.right(90)
+board.forward(100) 
+board.right(90)
+turtle.done()
